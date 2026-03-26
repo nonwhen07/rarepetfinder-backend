f5c368dabddde960dbc10596aeee31e87a92be27
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2026-03-27 06:40:31 +0800

    init backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+app.use(cors());
+app.use(jsonServer.bodyParser);
+
+// ⚠️ auth 要放在 router 前面
+app.use(auth);
+app.use(router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
